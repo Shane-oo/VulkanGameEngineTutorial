50f25977cce09fc1fb60c207f094cc6b0cff0122
Shane Monck <shane@constructivesoftware.com.au>
2024-04-19 14:34:31 +0800

    Per Fragment lighting

--- VulkanGameEngine/shaders/simple_shader_frag.glsl
+++ VulkanGameEngine/shaders/simple_shader_frag.glsl
@@ -1,8 +1,17 @@
 #version 450
 
 layout (location = 0) in vec3 fragColour;
+layout (location = 1) in vec3 fragPositionWorld;
+layout (location = 2) in vec3 fragNormalWorld;
 
 layout (location = 0) out vec4 outColour;
+
+layout (set = 0, binding = 0) uniform GlobalUbo {
+    mat4 projectionViewMatrix;
+    vec4 ambientLightColour;
+    vec3 lightPosition;
+    vec4 lightColour;
+} ubo;
 
 layout (push_constant) uniform Push {
     mat4 modelMatrix;
@@ -11,5 +20,12 @@ layout (push_constant) uniform Push {
 
 
 void main() {
-    outColour = vec4(fragColour, 1.0);
+    vec3 directionToPointLight = ubo.lightPosition - fragPositionWorld;
+    float attenuation = 1.0f / dot(directionToPointLight, directionToPointLight); // distance squared
+
+    vec3 lightColour = ubo.lightColour.xyz * ubo.lightColour.w * attenuation;
+    vec3 ambientLight = ubo.ambientLightColour.xyz * ubo.ambientLightColour.w;
+    vec3 diffuseLight = lightColour * max(dot(normalize(fragNormalWorld), normalize(directionToPointLight)), 0);
+
+    outColour = vec4((diffuseLight + ambientLight) * fragColour, 1.0);
 }
--- VulkanGameEngine/shaders/simple_shader_vert.glsl
+++ VulkanGameEngine/shaders/simple_shader_vert.glsl
@@ -6,6 +6,8 @@ layout (location = 2) in vec3 normal;
 layout (location = 3) in vec2 uv;
 
 layout (location = 0) out vec3 fragColour;
+layout (location = 1) out vec3 fragPositionWorld;
+layout (location = 2) out vec3 fragNormalWorld;
 
 layout (set = 0, binding = 0) uniform GlobalUbo {
     mat4 projectionViewMatrix;
@@ -24,14 +26,7 @@ void main() {
 
     gl_Position = ubo.projectionViewMatrix * positionWorld; 
 
-    vec3 normalWorldSpace = normalize(mat3(push.normalMatrix) * normal);
-
-    vec3 directionToPointLight = ubo.lightPosition - positionWorld.xyz;
-    float attenuation = 1.0f / dot(directionToPointLight, directionToPointLight); // distance squared
-    
-    vec3 lightColour = ubo.lightColour.xyz * ubo.lightColour.w * attenuation;
-    vec3 ambientLight = ubo.ambientLightColour.xyz * ubo.ambientLightColour.w;
-    vec3 diffuseLight = lightColour * max(dot(normalWorldSpace, normalize(directionToPointLight)), 0);
-
-    fragColour = (diffuseLight + ambientLight) * color;
+    fragNormalWorld = normalize(mat3(push.normalMatrix) * normal);
+    fragPositionWorld = positionWorld.xyz;
+    fragColour = color;
 }
